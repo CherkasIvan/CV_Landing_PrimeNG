7b1f112409cbe2d65241638fef908819a85c5ff5
JV13 <cherkas.ivan13@gmail.com>
2025-08-27 17:20:17 +0300

    rEFORM CODEDT

--- src/UI/connection-modal/conection-modal.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'cv-conection-modal',
-  imports: [],
-  templateUrl: './conection-modal.html',
-  styleUrl: './conection-modal.scss',
-})
-export class ConnectionModalComponent {}
--- src/UI/connection-modal/connection-modal.component.ts
+++ src/UI/connection-modal/connection-modal.component.ts
@@ -1,9 +1,20 @@
 import { Component } from '@angular/core';
+import { Dialog } from 'primeng/dialog';
+import { ButtonModule } from 'primeng/button';
+import { InputTextModule } from 'primeng/inputtext';
+import { AvatarModule } from 'primeng/avatar';
 
 @Component({
   selector: 'cv-connection-modal',
-  imports: [],
+  standalone: true,
+  imports: [Dialog, ButtonModule, InputTextModule, AvatarModule],
   templateUrl: './connection-modal.component.html',
   styleUrl: './connection-modal.component.scss',
 })
-export class ConnectionModal {}
+export class ConnectionModalComponent {
+  visible: boolean = false;
+
+  showDialog() {
+    this.visible = true;
+  }
+}
